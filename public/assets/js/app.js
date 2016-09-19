9059a5e0e09fd0bcbd45e5f0ec4e03d417840a20
Mike Sprague <mikesprague@gmail.com>
2016-09-18 21:32:45 -0400

    Change link to source

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,7 +3,7 @@ Vue.component( "footer-content", {
       <p class="text-muted text-center"> \
         Built by <a href="https://mikesprague.me" target="_blank">Mike Sprague</a>. \
         <br> \
-        Source <i class="fa fa-code"></i> available on <a href="https://github.com/mikesprague/vuejs-projects/tree/master/todo-list/public" target="_blank"><i class="fa fa-github"></i> GitHub</a>. \
+        Source <i class="fa fa-code"></i> available on <a href="https://github.com/mikesprague/vuejs-todo-list" target="_blank"><i class="fa fa-github"></i> GitHub</a>. \
       </p> \
     </div>'
 });
